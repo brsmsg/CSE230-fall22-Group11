ac6351c34506d03bc1f805b450928945537ea715
yirunzhao <yirunzhao@gmail.com>
2022-11-21 18:15:09 -0800

    init: basic ui

--- src/Shaft.hs
+++ src/Shaft.hs
@@ -1,8 +1,57 @@
-module Shaft where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Shaft
+  ( initGame
+  , Game(..)
+  , height
+  , width
+  , platform
+  , health
+  , score
+  , dead
+) where
+-- module Shaft where
 
-data Game = Game {} deriving (Show)
+import Control.Lens hiding ((<|), (|>), (:>), (:<))
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.State
+import Control.Monad.Extra (orM)
+import Data.Sequence (Seq(..), (<|))
+import qualified Data.Sequence as S
+import Linear.V2 (V2(..), _x, _y)
+import System.Random (Random(..), newStdGen)
+
+
+height :: Int
+height = 20
+
+width :: Int
+width = 20
+
+type Coord = V2 Int
+
+data Game = Game {
+  -- _player :: 
+  _score :: Int,
+  _health :: Int,
+  _dead :: Bool,
+  _pause :: Bool,
+  -- _platform :: [Coord]
+  _platform :: Coord
+} deriving (Show)
+
+
+makeLenses ''Game
+
 
 initGame :: IO Game
 initGame = do 
-  let g = Game {}
+  let g = Game {
+      _score = 0
+    , _dead = False
+    , _health = 10
+    , _platform = V2 10 10
+
+  }
   return g
--- src/UI.hs
+++ src/UI.hs
@@ -1,8 +1,8 @@
 module UI where
 
-import Shaft
+import Shaft 
 import Brick
-import Brick.BChan
+-- import Brick.BChan
 
 import Control.Monad (forever, void)
 import Control.Concurrent (forkIO)
@@ -11,6 +11,15 @@ import qualified Graphics.Vty as V
 import Brick.Widgets.Center (center)
 import Brick.Widgets.Border (vBorder)
 
+import Brick.BChan (newBChan, writeBChan)
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Border.Style as BS
+import qualified Brick.Widgets.Center as C
+import Control.Lens ((^.))
+import qualified Graphics.Vty as V
+import Data.Sequence (Seq)
+import qualified Data.Sequence as S
+import Linear.V2 (V2(..))
 -- custom event fed into the app constantly
 data Tick = Tick
 
@@ -26,6 +35,8 @@ app = App { appDraw = drawUI
           , appStartEvent = return ()
           , appAttrMap = const theMap
           }
+
+data Cell = Player | NormalPlatform | Empty
 
 main :: IO ()
 main = do
@@ -43,14 +54,61 @@ handleEvent (VtyEvent (V.EvKey V.KEsc [])) = halt
 handleEvent _ = continueWithoutRedraw 
 
 drawUI :: Game -> [Widget Name]
-drawUI g = [center (str"Left") <+> vBorder <+> center(str "Right")]
+-- drawUI g = [center (str"Left") <+> vBorder <+> center(str "Right")]
+drawUI g = [ C.center $ padRight (Pad 2) (drawStats g) <+> drawGrid g ]
 
--- drawStats :: Game -> Widget Name
--- drawStats g = hLimit 20
---   $ vBox []
+drawStats :: Game -> Widget Name
+drawStats g = hLimit 11
+  $ vBox [ drawScore (g ^. score)
+         , padTop (Pad 2) $ drawGameOver (g ^. dead)
+         ]
 
--- drawGrid :: Game -> Widget Name
--- drawGrid = undefined
+drawGameOver :: Bool -> Widget Name
+drawGameOver dead =
+  if dead
+     then withAttr gameOverAttr $ C.hCenter $ str "GAME OVER"
+     else emptyWidget
+
+
+drawGrid :: Game -> Widget Name
+drawGrid g = withBorderStyle BS.unicodeBold
+  $ B.borderWithLabel (str "NS-SHAFT")
+  $ vBox rows
+  where
+    rows         = [hBox $ cellsInRow r | r <- [height - 1, height - 2..0]]
+    cellsInRow y = [drawCoord (V2 x y) | x <- [0..width - 1]]
+    drawCoord    = drawCell . cellAt
+    cellAt c
+      -- | c `elem` g ^. 
+      | c == g ^. platform = NormalPlatform
+      | otherwise = Empty
+
+
+
+drawCell :: Cell -> Widget Name
+drawCell NormalPlatform = withAttr normalPlatformAttr cw
+drawCell Empty          = withAttr emptyAttr cw
+
+
+drawScore :: Int -> Widget Name
+drawScore n = withBorderStyle BS.unicodeBold
+  $ B.borderWithLabel (str "Score")
+  $ C.hCenter
+  $ padAll 1
+  $ str $ show n
+
+cw :: Widget Name
+cw = str " "
+
 
 theMap :: AttrMap
-theMap = attrMap V.defAttr []
+theMap = attrMap V.defAttr [(normalPlatformAttr, V.red `on` V.red)]
+
+emptyAttr :: AttrName
+emptyAttr = attrName "emptyAttr"
+
+normalPlatformAttr :: AttrName
+normalPlatformAttr = attrName "normalPlatformAttr"
+
+gameOverAttr :: AttrName
+gameOverAttr = attrName "gameOver"
